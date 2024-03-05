d00f3116f2e0ea1093c95df7586f3269075dd62e
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2024-03-05 11:54:30 +0100

    Removed secret key (I will not reuse it. I've generated a new one) Work done on CSS, layout

--- todo_app_project/todo_app/models.py
+++ todo_app_project/todo_app/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class TaskList(models.Model):
 	name = models.CharField(max_length=64)
 	date = models.DateField(auto_now=True)
-	user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="tasklist")
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasklist")
 		
 	def __str__(self):
 		return f"{self.name}: {self.date}"
